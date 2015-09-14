be9c858768371952abe670562b31edc9fe5f3f0f
Liu Yang <zation1@gmail.com>
2015-09-15 00:32:52 +0800

    include livereload

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: {
     admin: './admin/index.js',
     consumer: './consumer/index.js'
   },
+  devServer: {
+    hot: true,
+    inline: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
