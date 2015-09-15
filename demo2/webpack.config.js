b1073522da4ac7668d01a6990f8c04f0879060ca
Liu Yang <zation1@gmail.com>
2015-09-16 00:46:27 +0800

    use -p instead of uglify plugin

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,32 +1,15 @@
 var path = require('path');
-var webpack = require('webpack');
-var argv = require('node-argv');
-
-var args = argv(process.argv.slice(2)).options;
 
 var config = {
   entry: {
     admin: './admin/index.js',
     consumer: './consumer/index.js'
   },
-  devServer: {
-    inline: true
-  },
-  plugins: [],
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: '[name].bundle.js'
   }
 };
-
-if(args.minify) {
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    },
-    sourceMap: true
-  }));
-}
 
 module.exports = config;
