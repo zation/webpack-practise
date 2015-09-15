e0b1d156bbc026480a8f6b82bd655cdbe83e9bb8
Liu Yang <zation1@gmail.com>
2015-09-15 11:00:28 +0800

    minify based on argv

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,21 +1,32 @@
 var path = require('path');
 var webpack = require('webpack');
+var argv = require('node-argv');
 
-module.exports = {
+var args = argv(process.argv.slice(2)).options;
+
+var config = {
   entry: {
     admin: './admin/index.js',
     consumer: './consumer/index.js'
   },
   devServer: {
-    hot: true,
     inline: true
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [],
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: '[name].bundle.js'
   }
 };
+
+if(args.minify) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    sourceMap: true
+  }));
+}
+
+module.exports = config;
